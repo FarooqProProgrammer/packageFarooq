2eb89cea3b66407b8f67dec9003e1555b0486bc8
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2022-08-16 08:38:29 -0700

    Add files via upload

---
+++ packageFarooq/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="packageFarooq"
+      ,version="0.2",
+      description="This is Farooq package",
+      long_description="This is a very very Long Description",
+      author="Farooq",
+      url="https://github.com/FarooqProProgrammer/packageFarooq",
+      packages=['packageFarooq'],
+      install_requires=[]
+      )
